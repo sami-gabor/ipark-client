0cfa8d211b1cc8fbf07149b4d379acccfb388b6b
Samoila Gabor <samoila.gabor@iquestgroup.com>
2020-01-31 08:29:28 +0200

    Updates

---
+++ precache-manifest.492fc4b78b5ee1980279c6e14ee40c60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20185de7cfc0636db5c2a8ecd280ff9d",
+    "url": "/ipark-client/index.html"
+  },
+  {
+    "revision": "2c29b6c0748b5b40c8f9",
+    "url": "/ipark-client/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "75ca7b59678741e538fa",
+    "url": "/ipark-client/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2c29b6c0748b5b40c8f9",
+    "url": "/ipark-client/static/js/2.610f02be.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/ipark-client/static/js/2.610f02be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75ca7b59678741e538fa",
+    "url": "/ipark-client/static/js/main.c3eb82a6.chunk.js"
+  },
+  {
+    "revision": "3897a8bf67e3e546111c",
+    "url": "/ipark-client/static/js/runtime-main.da096f54.js"
+  }
+]);

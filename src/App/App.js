0efb1416b2a72e143c93baf990bcb7d431a98299
Samoila Gabor <samoila.gabor@iquestgroup.com>
2020-02-07 11:31:35 +0200

    Move BrowserRouter from index to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import '../App.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -9,20 +9,17 @@ class App extends Component {
   render() {
     const App = () => (
       <div>
-        <Switch>
-          <Route exact path='/' component={Login}/>
-          <Route path='/login' component={Login}/>
-          <Route path='/register' component={Register}/>
-          <Route path='/home' component={Home}/>
-        </Switch>
-        yugyu
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Login}/>
+            <Route path='/login' component={Login}/>
+            <Route path='/register' component={Register}/>
+            <Route path='/home' component={Home}/>
+          </Switch>
+        </BrowserRouter>
       </div>
     )
-    return (
-      // <Switch>
-        <App/>
-      // </Switch>
-    );
+    return <App/>;
   }
 }
 
